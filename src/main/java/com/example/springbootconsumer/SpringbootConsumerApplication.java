b7fd4a506e33151e9c022fe003b7600a6ddea9e2
dragon <596372190@qq.com>
2022-06-28 09:19:25 +0800

    first commit

---
+++ src/main/java/com/example/springbootconsumer/SpringbootConsumerApplication.java
@@ -0,0 +1,26 @@
+package com.example.springbootconsumer;
+
+import com.gupaoedu.book.dubbo.IHelloService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootConsumerApplication {
+
+    @Reference(url = "dubbo://192.168.56.1:20880/com.gupaoedu.book.dubbo.IHelloService")
+    private IHelloService helloService;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootConsumerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(){
+        return args -> System.out.println(helloService.sayHello("Mic"));
+    }
+
+}
